571512814743344c4218d2b76586b4b9a03f1e8e
moqada <moqada@gmail.com>
2015-10-03 20:26:13 +0900

    Checkpoint 8

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -26,6 +26,16 @@ class RatingControl: UIView {
         }
     }
 
+    override func layoutSubviews() {
+        var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
+        
+        // Offset each button's origin by the length of the button plus spacing.
+        for (index, button) in ratingButtons.enumerate() {
+            buttonFrame.origin.x = CGFloat(index * (44 + 5))
+            button.frame = buttonFrame
+        }
+    }
+
     // MARK: Button Action
     func ratingButtonTapped(button: UIButton) {
         print("Button pressed")
