bf2acbfe1d17bd874045a533dc2812823ac9a07f
moqada <moqada@gmail.com>
2015-10-03 20:19:45 +0900

    Checkpoint 6

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -15,6 +15,12 @@ class RatingControl: UIView {
         
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
         button.backgroundColor = UIColor.redColor()
+        button.addTarget(self, action: "ratingButtonTapped:", forControlEvents: .TouchDown)
         addSubview(button)
+    }
+
+    // MARK: Button Action
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed")
     }
 }
