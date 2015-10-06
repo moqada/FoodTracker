750be372d1b9493707ce6619deba80fd54ceb304
moqada <moqada@gmail.com>
2015-10-06 14:24:50 +0900

    feat(TodoApp/view): Implement TableView

--- TodoApp/TodoListViewController.swift
+++ TodoApp/TodoListViewController.swift
@@ -7,21 +7,31 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     // MARK: Properties
     @IBOutlet weak var tableView: UITableView!
+    var todoEntities: [Todo]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        todoEntities = Todo.MR_findAll() as? [Todo]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoEntities.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("TodoListItem")!
+        cell.textLabel!.text = todoEntities[indexPath.row].item
+        return cell
+    }
 }
 
