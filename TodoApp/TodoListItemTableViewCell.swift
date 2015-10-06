c12dbd2d89a081e33ac7f314e22e1d1096b99fd8
moqada <moqada@gmail.com>
2015-10-06 11:34:26 +0900

    feat(TodoApp/view): Add TodoListItemTableViewCell.swift

---
+++ TodoApp/TodoListItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TodoListItemTableViewCell.swift
+//  TodoApp
+//
+//  Created by Masahiko Okada on 2015/10/06.
+//
+//
+
+import UIKit
+
+class TodoListItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
