1522828bfc90acc2161ccb72b79bdcc50d3655ba
moqada <moqada@gmail.com>
2015-10-06 11:19:47 +0900

    feat(TodoApp/header): Add header file and setup AppDelegate.swift
    - MagicalRecord の Bridge 用ヘッダファイルを追加
    - AppDelegate に初期化設定

---
+++ TodoApp/TodoApp-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  TodoApp-Bridging-Header.h
+//  TodoApp
+//
+//  Created by Masahiko Okada on 2015/10/06.
+//
+//
+
+#ifndef TodoApp_TodoApp_Bridging_Header_h
+#define TodoApp_TodoApp_Bridging_Header_h
+
+#import "CoreData+MagicalRecord.h"
+
+#endif /* TodoApp_Bridging_Header_h */
