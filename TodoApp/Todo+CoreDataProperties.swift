9c2abac5f5b6ebc69387b9465e376593507068e7
moqada <moqada@gmail.com>
2015-10-06 14:11:52 +0900

    feat(TodoApp/model): Add Todo model

---
+++ TodoApp/Todo+CoreDataProperties.swift
@@ -0,0 +1,19 @@
+//
+//  Todo+CoreDataProperties.swift
+//  TodoApp
+//
+//  Created by Masahiko Okada on 2015/10/06.
+//
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Todo {
+
+    @NSManaged var item: String?
+
+}
---
+++ TodoApp/Todo.swift
@@ -0,0 +1,16 @@
+//
+//  Todo.swift
+//  TodoApp
+//
+//  Created by Masahiko Okada on 2015/10/06.
+//
+//
+
+import Foundation
+import CoreData
+
+class Todo: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
